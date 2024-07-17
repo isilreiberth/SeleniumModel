0304a6f4d5234cc9dc0cbe40b9a4aa17ea0d170f
isilreiberth <isilreiberth@somosmakers.co>
2024-07-16 22:30:06 -0300

    Cambios Varios 2

---
+++ src/main/java/pageobjects/LostPasswordPage.java
@@ -0,0 +1,10 @@
+package pageobjects;
+
+import org.openqa.selenium.WebDriver;
+
+public class LostPasswordPage extends BasePage {
+    public LostPasswordPage(WebDriver driver) {
+        super(driver);
+    }
+}
+
